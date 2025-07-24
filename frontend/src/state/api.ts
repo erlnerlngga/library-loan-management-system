e4e7ca33c865796b83a661be54f4c9e0705428f9
erlnerlngga <erlanerlangg@gmail.com>
2025-07-25 00:20:43 +0800

    add url api

--- frontend/src/state/api.ts
+++ frontend/src/state/api.ts
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_API_BASE_URL,
+    baseUrl: import.meta.env.VITE_API_BASE_URL_PROD,
     // prepareHeaders: async (headers) => {
     //   const token = Cookies.get("token");
     //   if (token) {
