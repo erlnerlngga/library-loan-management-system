cbce8257061eb4bf47d9844f20f6579f08aeb6ae
erlnerlngga <erlanerlangg@gmail.com>
2025-07-25 00:26:25 +0800

    add cors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -36,6 +36,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://103.74.5.105:8000",
+    "http://103.74.5.105:3000",
     "http://localhost",
     "http://127.0.0.1",
 ]
